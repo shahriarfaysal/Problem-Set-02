a7fd80ca6dfd93d9ef5dd416ab01163a3098007a
Shahriar Faysal <160359045+shahriarfaysal@users.noreply.github.com>
2024-03-01 01:47:41 +0600

    Add files via upload

---
+++ continue.c
@@ -0,0 +1,14 @@
+#include <stdio.h>
+int main()
+{
+    int i,n;
+    for(i=1;i<=10;i++)
+    {
+        if(i==5)
+        {
+            continue;
+        }
+        printf("%d\n",i);
+    }
+    return 0;
+}
